5ed6c87e3924a6ea0bbf916b479fccd4b133c180
rizfanradya <rizfankusuma@gmail.com>
2024-02-11 23:20:52 +0700

    navbar

--- src/app/(pages)/home/page.jsx
+++ src/app/(pages)/home/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
 
 const linkItems = [
   { name: "Home", href: "/" },
@@ -19,30 +20,50 @@ const sosmedList = [
 export default function Home() {
   return (
     <>
-      <div className="flex justify-around items-center py-4 fixed w-screen bg-slate-800">
-        <h1 className="font-semibold text-xl">
-          <Link href={"/"}>OODK</Link>
-        </h1>
-
-        <ul className="flex gap-6 items-center">
-          {linkItems.map((doc) => (
-            <li key={doc.name} className="hover:text-sky-500 transition">
-              <Link href={doc.href}>{doc.name}</Link>
-            </li>
-          ))}
-        </ul>
+      <div className="navbar bg-slate-800 fixed">
+        <div className="navbar-start">
+          <Link href={"/"} className="btn btn-ghost text-xl">
+            OODK
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {linkItems.map((doc) => (
+              <li key={doc.name}>
+                <Link href={doc.href}>{doc.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <IoMenu size={"2em"} />
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {linkItems.map((doc) => (
+                <li key={doc.name}>
+                  <Link href={doc.href}>{doc.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
 
-      <div className="h-screen w-screen flex items-center justify-around">
-        <div className="max-w-2xl flex flex-col gap-1">
-          <h1 className="font-semibold text-5xl">Hai Saya OODK</h1>
+      <div className="h-screen flex flex-col lg:flex-row items-center justify-around pt-20 p-4">
+        <div className="max-w-2xl flex flex-col gap-1 order-2 lg:order-1 text-center lg:text-start">
+          <h1 className="font-semibold text-4xl">Hai Saya OODK</h1>
           <h3 className="text-sky-500 font-semibold text-2xl">
             Frontend Developer
           </h3>
           <p className="my-5">
             saya adalah ahli programmer,menjadi Backend Developer terhandal
           </p>
-          <div className="flex gap-4 items-center">
+          <div className="flex gap-4 items-center m-auto lg:m-0">
             <Link href={"/"} className="btn btn-info">
               Hire Me
             </Link>
@@ -50,7 +71,7 @@ export default function Home() {
               Let&apos;s Talk
             </Link>
           </div>
-          <div className="flex items-center gap-3 mt-24">
+          <div className="flex items-center gap-3 lg:mt-24 mt-10 m-auto lg:m-0">
             {sosmedList.map((doc) => (
               <Link
                 target="blank"
@@ -63,14 +84,13 @@ export default function Home() {
             ))}
           </div>
         </div>
-
-        <div className="overflow-hidden rounded-b-full rounded-s-full">
+        <div className="overflow-hidden lg:rounded-b-full lg:rounded-s-full lg:rounded-none rounded-full order-1 lg:order-2">
           <Image
             src={"/profile.jpeg"}
             alt="profile"
             width={200}
             height={200}
-            className="w-80"
+            className="lg:w-80 w-52"
           />
         </div>
       </div>
@@ -92,6 +112,8 @@ export default function Home() {
           Read More
         </Link>
       </div>
+
+      <div></div>
     </>
   );
 }
