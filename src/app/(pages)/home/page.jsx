e7a3e1927cfc853f9f941e84871e83d103fec84e
rizfanradya <rizfankusuma@gmail.com>
2024-02-11 23:52:16 +0700

    my journey

--- src/app/(pages)/home/page.jsx
+++ src/app/(pages)/home/page.jsx
@@ -113,7 +113,61 @@ export default function Home() {
         </Link>
       </div>
 
-      <div></div>
+      <div className="px-4 py-10">
+        <h1 className="font-semibold text-4xl pb-10 text-center">
+          My <span className="text-sky-500">Journey</span>
+        </h1>
+        <div className="flex flex-wrap justify-center gap-10">
+          <div>
+            <p className="font-semibold text-xl pb-4 text-center">Education</p>
+            <ul className="steps steps-vertical">
+              <li className="step step-info">
+                <div className="card border-2 border-sky-600 shadow-xl">
+                  <div className="card-body">
+                    <p className="flex items-center gap-2 text-sm text-info">
+                      <span className="badge badge-info"></span> 20XX - 20XX
+                    </p>
+                    <h2 className="card-title text-base">
+                      SMAN X ***************
+                    </h2>
+                    <p className="text-start text-sm">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nisi, dolorum.
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className="font-semibold text-xl pb-4 text-center">Experience</p>
+            <ul className="steps steps-vertical">
+              <li className="step step-info">
+                <div className="card border-2 border-sky-600 shadow-xl">
+                  <div className="card-body">
+                    <p className="flex items-center gap-2 text-sm text-info">
+                      <span className="badge badge-info"></span> 20XX - 20XX
+                    </p>
+                    <h2 className="card-title text-base">
+                      SMAN X ***************
+                    </h2>
+                    <p className="text-start text-sm">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Nisi, dolorum.
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="px-4 py-10 bg-slate-700">
+        <h1 className="font-semibold text-4xl pb-10 text-center">
+          My <span className="text-sky-500">Skills</span>
+        </h1>
+      </div>
 
       <footer className="footer footer-center p-4 bg-base-300 text-base-content">
         <aside>
